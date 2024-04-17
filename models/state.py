4eabda944515d5bfaffdcbcf163f12c00a7860e1
et.touil <et.touil505@gmail.com>
2024-04-18 00:49:05 +0100

    Update models/state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,10 @@
-#!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from sqlalchemy import String
 
-
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """This class represents a state"""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade='all, delete-orphan', backref='state')
