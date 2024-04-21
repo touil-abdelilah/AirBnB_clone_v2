b682c7a129f893ba48f5e3e72c514f3bd0872f1f
touil-abdelilah <et.touil505@gmail.com>
2024-04-21 03:13:00 +0100

    states

--- models/state.py
+++ models/state.py
@@ -1,11 +1,35 @@
 #!/usr/bin/python3
-from sqlalchemy import Column
-from sqlalchemy.orm import relationship
+"""state class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import String
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+import models
+from models.city import City
+import shlex
+
 
 class State(BaseModel, Base):
-    """This class represents a state"""
-    __tablename__ = 'states'
+    """class State
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade='all, delete-orphan', backref='state')
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
+
+    @property
+    def cities(self):
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
